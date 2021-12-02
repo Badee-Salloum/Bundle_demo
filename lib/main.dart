9a002de1d4ab908dc4e6901b5c12ab238bc8d036
Badee Salloum <Bade33sm@gmail.com>
2021-12-02 17:48:46 +0200

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Container(
+          color: Colors.white,
+        ),
+      ),
+    );
   }
 }
