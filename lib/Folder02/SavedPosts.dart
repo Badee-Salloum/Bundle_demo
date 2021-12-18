928bf2b2981f6f39a930f8309c82a6cd10e52c2d
badee <badee33sm@gmail.com>
2021-12-19 01:57:09 +0200

    commit add .

---
+++ lib/Folder02/SavedPosts.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SavedPosts extends StatefulWidget {
+  const SavedPosts({Key? key}) : super(key: key);
+
+  @override
+  _SavedPostsState createState() => _SavedPostsState();
+}
+
+class _SavedPostsState extends State<SavedPosts> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Saved posts',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset('assets/Photos/No saved posts yet.svg'),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'No Saved posts yet',
+              style: TextStyle(color: Colors.black26),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
