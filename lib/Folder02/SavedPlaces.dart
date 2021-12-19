69bc0b349376c3bce3bddbc9ae9a8139d0ab1630
badee <badee33sm@gmail.com>
2021-12-20 00:41:28 +0200

    commit add .

---
+++ lib/Folder02/SavedPlaces.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: file_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SavedPlaces extends StatefulWidget {
+  const SavedPlaces({Key? key}) : super(key: key);
+
+  @override
+  _SavedPlacesState createState() => _SavedPlacesState();
+}
+
+class _SavedPlacesState extends State<SavedPlaces> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Saved places',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset('assets/Photos/No saved places yet.svg'),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'No Saved posts yet',
+              style: TextStyle(color: Colors.black26),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
