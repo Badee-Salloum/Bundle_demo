9ad65be793336e1a76c4d5a05634b9ee62c3f645
walaa-94863 <walaa.alkdamani.1998@gmail.com>
2022-01-09 05:50:18 +0200

    git add .

--- lib/Folder02/looginInfo.dart
+++ lib/Folder02/looginInfo.dart
@@ -1,4 +1,5 @@
 import 'package:bundle_demo/Folder02/setting_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../icomoon_icons.dart';
@@ -51,13 +52,54 @@ class _LoginInfoScreenState extends State<LoginInfoScreen> {
           children: [
             Stack(
               children: [
-                Container(
+                Padding(
+                  padding: EdgeInsets.all(20),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
                     child: Image(
-                      height: 300,
+                      height: 250,
+                      width: double.infinity,
                       fit: BoxFit.fill,
                       image: AssetImage('assets/Photos/Traffic night.jpg'),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, bottom: 20, right: 20),
+                    child: Container(
+                      padding: EdgeInsets.only(left: 60),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(30, 166, 166, 166),
+                        borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(20.0),
+                          bottomLeft: Radius.circular(20.0),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Damascus',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          Text('1 day ago',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          Text('huawei nova 4',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        ],
+                      ),
                     ),
                   ),
                 ),
@@ -68,20 +110,27 @@ class _LoginInfoScreenState extends State<LoginInfoScreen> {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30.0),
                       ),
-                      height: 51.0,
-                      minWidth: 300.0,
+                      height: 60.0,
+                      minWidth: 100.0,
                       color: Color(0xff9676FF),
                       child: Text(
-                        'Save',
+                        'Logout',
                         style: TextStyle(
                           fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
                       ),
                       onPressed: () {}),
-                  bottom: 10,
-                  right: 10,
-                )
+                  bottom: 40,
+                  right: 30,
+                ),
+                Positioned(
+                  child: Icon(Icomoon.location_outline,
+                      color: Colors.white, size: 40),
+                  bottom: 50,
+                  left: 30,
+                ),
               ],
             ),
           ],
